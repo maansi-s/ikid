ddb5498809063e16480ec7a9ed374f285141ea3a
msurve2@uw.edu <msurve2@uw.edu>
2025-04-27 18:11:11 -0700

    Initial Commit

---
+++ iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by Maansi Surve on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iKidTests/iKidTests.swift
@@ -0,0 +1,17 @@
+//
+//  iKidTests.swift
+//  iKidTests
+//
+//  Created by Maansi Surve on 4/27/25.
+//
+
+import Testing
+@testable import iKid
+
+struct iKidTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
